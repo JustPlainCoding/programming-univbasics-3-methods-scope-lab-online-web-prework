02831f639289dbb4033b8fccff266537c44d21ca
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-13 15:41:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,13 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "It's-a-me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+end
+
+def link
+  status = "It's Dangerous T"
 end
